5cb0ed9f4e59cc4152cd36f9361e749d13f9ecd1
shel3over <tahmi.hamza@gmail.com>
2013-09-15 10:58:10 +0100

    add configuration file close #1

--- main.py
+++ main.py
@@ -20,6 +20,11 @@ import subprocess
 import smtplib
 import os
 import urllib
+import ConfigParser
+
+config = ConfigParser.ConfigParser()
+config.read('config.conf')
+
 
 def actionManager():
 	#i'm a thread i loop forever :)
@@ -27,15 +32,14 @@ def actionManager():
 		ip,line=actionQueue.get()
 		actionQueue.task_done()
 		actionIptable(ip)
-
-		fromaddr = SMTP_FROM_MAIL
-		toaddrs  = SMTP_TO 
+		continue
+		#sending mail alert :)
 		msg=MIMEText(line)
 		msg['Subject'] = 'KIDS ALERT : ', ip
-		msg['From'] = fromaddr
-		msg['To'] = ', '.join(toaddrs)
-		server =  smtplib.SMTP(SMTP_SERVER,SMTP_PORT)
-		server.login(SMTP_LOGIN,SMTP_PASS)
+		msg['From'] = config.get('smtp','from')
+		msg['To'] = config.get('smtp','to')
+		server =  smtplib.SMTP(config.get('smtp','server'),config.get('smtp','port'))
+		server.login(config.get('smtp','user'),config.get('smtp','pass'))
 		server.sendmail(fromaddr,toaddrs, msg.as_string())
 		server.quit()
 
@@ -45,7 +49,7 @@ def logWatcher():
 	for item in open('blacklist').readlines():
 		blacklist.append(item.strip())
 
-	f = subprocess.Popen(['tail','-F',LOG_PATH],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+	f = subprocess.Popen(['tail','-F',config.get('global','logpath')],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 	lastIp=''
 	#i'm a thread i loop forever :)
 	while True:
@@ -55,14 +59,14 @@ def logWatcher():
 			continue
 		for bad in blacklist:
 			if bad in line:
-				print 'baaaaaaaaaad',bad
+				print 'baaaaaaaaaad',bad#just for debug
 				print line
 				lastIp=ip
 				actionQueue.put((ip,line))
 				break
 
 def actionCloudflare(ip):
-	urllib.open('https://www.cloudflare.com/api.html?a=ban&key=%s&u=xxxxxxxxxxxxx@gmail.com&tkn=xxxxxxxxxxxxxxxxxxxxxxxxxxx'%ip).read()
+	urllib.open('https://www.cloudflare.com/api.html?a=ban&key=%s&u=%s&tkn=%s'%(ip,config.get('cloudflare','user'),config.get('cloudflare','token'))).read()
 
 def actionIptable(ip):
 	os.popen('/usr/bin/env iptables -A INPUT -s %s -j DROP'%ip)
